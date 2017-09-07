912abc46da77e910c03f52208e987dd5c7258444
Lena Faure <lena.faure@gmail.com>
2017-09-07 17:09:54 +0200

    Added user euthentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # before any action performed by this controller, verify if the user is authenticated
+  before_action :authenticate_user!, :except => [:pages, :home]
 end
