f41dfbc1afc5b2a21ffbf20730005ce9551157a2
Lena Faure <lena.faure@gmail.com>
2017-09-07 22:38:22 +0200

    A user is redirected to profile page after sign in / sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
 
   # before any action performed by this controller, verify if the user is authenticated
   before_action :authenticate_user!, :except => [:pages, :home]
+
+  def after_sign_in_path_for(resource)
+    edit_user_path(current_user)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170907143749) do
+ActiveRecord::Schema.define(version: 20170907195936) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
