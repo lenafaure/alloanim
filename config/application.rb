2e84a662ea9f814ab4900249cdda2a4327397618
Lena Faure <lena.faure@gmail.com>
2017-09-12 15:29:30 +0200

    Adding validations to sign_up form

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,10 @@ module AnimExpress
     # -- all .rb files in that directory are automatically loaded.
 
     config.i18n.default_locale = :fr
+
+    # Prevent change of css when validation errors
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+      html_tag
+    }
   end
 end
