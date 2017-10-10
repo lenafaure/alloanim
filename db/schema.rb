0668ba45649341b71983e4b1a06e388cb8dc5d8f
Lena Faure <lena.faure@gmail.com>
2017-10-10 13:03:49 +0200

    Add Model Offers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005132723) do
+ActiveRecord::Schema.define(version: 20171010110206) do
 
   create_table "availabilities", force: :cascade do |t|
     t.integer "user_id"
@@ -53,6 +53,15 @@ ActiveRecord::Schema.define(version: 20171005132723) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_centre_loisirs_on_email", unique: true
     t.index ["reset_password_token"], name: "index_centre_loisirs_on_reset_password_token", unique: true
+  end
+
+  create_table "offers", force: :cascade do |t|
+    t.string "date"
+    t.string "time_slot"
+    t.string "diploma"
+    t.integer "offer_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
