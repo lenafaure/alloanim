60c6aa15500ad4fa25c39a0ff54156e0e914f63f
Lena Faure <lena.faure@gmail.com>
2017-10-26 11:05:44 +0200

    Deny authorization for User to see full Offers list

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,5 +1,6 @@
 class OffersController < ApplicationController
   before_action :set_offer, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
 
   # GET /offers
   # GET /offers.json
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can [:update, :edit, :show, :matching_offers], User, :id => user.id
-    can :read, Offer
-
+    cannot :read, Offer
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
