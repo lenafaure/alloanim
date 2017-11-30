f6c26cade29ba1b77579b3116e3e1e3a24ddbf23
Lena Faure <lena.faure@gmail.com>
2017-11-30 09:45:01 +0100

    Redirect to dedicated Home Page when users & centers logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
 
   def home
+    if user_signed_in?
+      redirect_to user_matching_offers_path(current_user)
+    end
+
+    if center_signed_in?
+      redirect_to users_path
+    end
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # root page of the application
+
   root 'pages#home'
 
   get '/search' => 'pages#search'
