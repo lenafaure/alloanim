32c7d95cf75161d6fd403e67f5d018822cad0a2e
Lena Faure <lena.faure@gmail.com>
2017-12-06 11:07:04 +0100

    Test Notification Mail for Matching

---
+++ config/schedule.rb
@@ -0,0 +1,10 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+every 1.day, :at => '11:10 am' do
+  rake "matches_notification"
+end
+
+# Learn more: http://github.com/javan/whenever
