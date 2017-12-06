1a16c8a3bbff435441b2bdb74f79de9097144952
Lena Faure <lena.faure@gmail.com>
2017-12-06 11:17:06 +0100

    Test Notification Mail for Matching

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,10 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
+every 1.minute do
+  rake "matches_notification"
+end
+
 every 1.day, :at => '11:10 am' do
   rake "matches_notification"
 end
