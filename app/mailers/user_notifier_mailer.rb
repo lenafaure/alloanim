4dc7904fcab7dcf6b277c205db0ebec82fd5f5b7
Lena Faure <lena.faure@gmail.com>
2017-12-06 10:27:39 +0100

    Test Notification Mail for Matching

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,13 +1,13 @@
 class UserNotifierMailer < ApplicationMailer
-  default :from => 'any_from_address@example.com'
+  default :from => 'alloanim@futur.paris'
   def send_matches_notification(user)
     @user = user
     @matches = User.offer_matches(@user)
     if @matches.present?
-    puts @matches
-    puts @user.email
-    mail( :to => @user.email,
-          :subject => 'You have a match' )
+      mail( :to => @user.email,
+            :subject => 'Vous avez ' + @matches.count.to_s + ' nouvelle(s) offre(s) sur AlloAnim' ) do |format|
+        format.html(content_transfer_encoding: "7bit")
+      end
     end
   end
 end
