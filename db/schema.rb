c8f3700096eaa99bb2f7026e967421074cf51fe6
Lena Faure <lena.faure@gmail.com>
2017-12-13 19:20:33 +0100

    Add Model Circonscription

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204085409) do
+ActiveRecord::Schema.define(version: 20171213161457) do
 
   create_table "availabilities", force: :cascade do |t|
     t.integer "user_id"
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 20171204085409) do
     t.index ["confirmation_token"], name: "index_centers_on_confirmation_token", unique: true
     t.index ["email"], name: "index_centers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_centers_on_reset_password_token", unique: true
+  end
+
+  create_table "circonscriptions", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "diplomas", force: :cascade do |t|
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
+  factory :circonscription do
+    name "MyString"
+  end
   factory :user do
     id '1'
     email 'test@example.com'
