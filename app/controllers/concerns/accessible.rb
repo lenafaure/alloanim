23a7202541477cc5c0f7324f9f6073e5478f8302
Lena Faure <lena.faure@gmail.com>
2017-12-23 11:28:31 +0100

    Redirect to Validations page on sign_in

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -8,13 +8,10 @@ module Accessible
   protected
   def check_user
     if current_center
-      # if you have rails_admin. You can redirect anywhere really
       redirect_to(users_path) && return
     elsif current_rhagent
-        # if you have rails_admin. You can redirect anywhere really
-        redirect_to(rhagents_path) && return
+        redirect_to(animateurs_path) && return
     elsif current_user
-      # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
       redirect_to(user_path(current_user)) && return
     end
   end
