885335fbbda3fa1f9274ac3aeb0356671f0ea3c7
Lena Faure <lena.faure@gmail.com>
2018-01-02 12:28:36 +0100

    Centers and Rhagents can read School

--- app/models/center_ability.rb
+++ app/models/center_ability.rb
@@ -5,9 +5,9 @@ class CenterAbility
     center ||= Center.new # guest user (not logged in)
     can :manage, Offer
     can :read, User
+    can :read, School
     can :show, Center, :id => center.id
     can :manage, Center
-    cannot :manage, School
     cannot :manage, Circonscription
     cannot :manage, Diploma
     cannot :manage, Rhagent
--- app/models/rhagent_ability.rb
+++ app/models/rhagent_ability.rb
@@ -4,10 +4,10 @@ class RhagentAbility
   def initialize(rhagent)
     rhagent ||= Rhagent.new # guest user (not logged in)
     can :read, Offer
+    can :read, School
     can :manage, User
     can :show, Rhagent, :id => rhagent.id
     can :manage, Rhagent
-    cannot :manage, School
     cannot :manage, Circonscription
     cannot :manage, Diploma
     cannot :manage, Center
